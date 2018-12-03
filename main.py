d8b93a0b34772ddd19f14868c18b69611f4f9e24
Hunter Johnson <hhjohnso@indiana.edu>
2018-12-02 22:36:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-# params = {location(s), url}
+# params = {location(s), click_nums, url(s)}
 
-# use Tor to set IP (custom bridge) for each click
+# use Tor to set IP (new node / [custom] bridge ) for each click
+
+
+def main():
+  
+  
