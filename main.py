b2ed7ac52721fa131fc6e2ca372d5f816ea1e69d
Hunter Johnson <hhjohnso@indiana.edu>
2018-12-10 14:37:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,24 @@
 # use Tor to set IP (new node / [custom] bridge ) for each click
 
 
+
+
 def main():
+  url = input(url)
+  time = input(time) # time will be randomly adjusted
+  click_num = input(clicknum)
+  locations = input(locations)
   
-  
+  while(click_num > 0)
+    loc = shuffle(locations)
+    x = new_tor_connection(loc) # or use bridge/custom nodes
+    x.navigate(url)
+    x.sleep(time)   # or x.random_click / browse
+    x.navigate(exit_url)
+    x.sleep(30)
+    x.close()
+    click_num -= 1
+    
+    
+if __name__ == "__main__":
+    main()
